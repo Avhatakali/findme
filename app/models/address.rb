1fa70e0655d97b1e4827cf5c3aea67280de56545
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-02-08 12:50:27 +0200

    third commit

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :number,:street, presence: true,
+                    length: { minimum: 4 }
+
+  validates :city, :suburb, presence: true,
+          length: { minimum: 2 }
+          
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_one :address, dependent: :destroy
+
+  validates :first_name,:last_name, presence: true,
+                    length: { minimum: 4 }
+
+  validates :age, presence: true,
+          length: { minimum: 1 }
+
+
 end
